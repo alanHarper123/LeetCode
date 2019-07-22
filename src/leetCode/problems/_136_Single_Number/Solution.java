320cd4d82e9068319febde248dfb369fc6a02a95
alanHarper123 <1531508001@qq.com>
2019-07-22 16:29:21 +0800

    136 copy best

---
+++ src/leetCode/problems/_136_Single_Number/Solution.java
@@ -0,0 +1,11 @@
+package leetCode.problems._136_Single_Number;
+
+public class Solution {
+    public int singleNumber(int[] nums) {
+    	int ans = 0;
+    	for (int i = 0; i < nums.length; i++) {
+			ans^=nums[i];
+		}
+        return ans;
+    }
+}
