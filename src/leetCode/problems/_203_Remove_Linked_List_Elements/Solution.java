169c4ff98f7303f8c813c23c826b14e3338dfe7b
alanHarper123 <1531508001@qq.com>
2019-07-29 19:06:20 +0800

    203 best

---
+++ src/leetCode/problems/_203_Remove_Linked_List_Elements/Solution.java
@@ -0,0 +1,23 @@
+package leetCode.problems._203_Remove_Linked_List_Elements;
+
+import leetCode.problems.commonUtil.ListNode;
+
+public class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode pre = null;
+        ListNode newHead = head;
+        while (head!=null) {
+        	if(head.val == val) {
+        		if(pre!=null) {
+        			pre.next = head.next;
+        		}
+        		if(newHead==head)
+        			newHead = head.next;	
+        	}else {
+        		pre = head;
+        	}
+        	head = head.next;	
+		}
+        return newHead;
+    }
+}
