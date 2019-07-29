a62759df6b85cad8bd1197fe0c56f55e634bebdb
alanHarper123 <1531508001@qq.com>
2019-07-29 16:02:06 +0800

    191 best can be improved by shifting n toward 0 using >>> logical shift;

---
+++ src/leetCode/problems/_191_Number_of_1_Bits/Solution.java
@@ -0,0 +1,15 @@
+package leetCode.problems._191_Number_of_1_Bits;
+
+public class Solution {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int count=0;
+        int k=1;
+        for (int i = 0; i < 32; i++) {
+			if((n&k)!=0)
+				count++;
+			k=k<<1;
+		}
+        return count;
+    }
+}
