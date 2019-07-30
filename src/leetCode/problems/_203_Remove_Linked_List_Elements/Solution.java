746aa38e5f65a43d43dee66150bbde832ac31624
alanHarper123 <1531508001@qq.com>
2019-07-30 11:58:56 +0800

    205 good

--- src/leetCode/problems/_203_Remove_Linked_List_Elements/Solution.java
+++ src/leetCode/problems/_203_Remove_Linked_List_Elements/Solution.java
@@ -19,5 +19,6 @@ public class Solution {
         	head = head.next;	
 		}
         return newHead;
+        
     }
 }
---
+++ src/leetCode/problems/_205_Isomorphic_Strings/Solution.java
@@ -0,0 +1,21 @@
+package leetCode.problems._205_Isomorphic_Strings;
+
+public class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        HashMap<Character, Character> base = new HashMap<>();
+        HashSet<Character> vals = new HashSet<>();
+        for (int i = 0; i < s.length(); i++) {
+			Character a = s.charAt(i);
+			Character b = t.charAt(i);
+			Character baseB = base.get(a);
+			if(baseB==null) {
+				base.put(a, b);
+				if(!vals.add(b))
+					return false;
+			}else if(baseB!=b) {
+				return false;
+			}
+		}
+        return true;
+    }
+}
