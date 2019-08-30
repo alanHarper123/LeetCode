728cf0bb8ea31bdd23d718c9f7215075498be9b5
alanHarper123 <1531508001@qq.com>
2019-08-30 20:47:06 +0800

    235 best

--- src/leetCode/problems/_235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree/Solution.java
+++ src/leetCode/problems/_235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree/Solution.java
@@ -3,24 +3,17 @@ package leetCode.problems._235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree;
 import leetCode.problems.commonUtil.TreeNode;
 
 class Solution {
-	private TreeNode result=null;
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        recursiveFind(root, p, q);
-        return result;
-    }
-    private void recursiveFind(TreeNode root, TreeNode p, TreeNode q) {
-    	if(result!=null||root==null)
-    		return;
-    	
-    	if((root.val>p.val&&root.val>q.val)) {
-    		recursiveFind(root.left, p, q);
-    		return;
-    	}
-    		
-    	if((root.val<p.val&&root.val<q.val)) {
-    		recursiveFind(root.right, p, q);
-    		return;
-    	}
-		result = root;
+        while (root!=null) {
+        	if((root.val>p.val&&root.val>q.val))
+        		root = root.left;
+        	else if((root.val<p.val&&root.val<q.val)) {
+        		root = root.right;
+        	}
+        	else {
+        		return root;
+        	}
+		}
+        return root;
     }
 }
