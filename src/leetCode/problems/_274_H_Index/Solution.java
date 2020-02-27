8fac1bdea2b7a8686eb49880d19597ff42ad6937
alanHarper123 <1531508001@qq.com>
2020-02-27 15:26:15 +0800

    274 best

---
+++ src/leetCode/problems/_274_H_Index/Solution.java
@@ -0,0 +1,16 @@
+package leetCode.problems._274_H_Index;
+
+import java.util.Arrays;
+
+public class Solution {
+    public int hIndex(int[] citations) {
+    	if(citations.length==0)
+    		return 0;
+        Arrays.sort(citations);
+        for (int i = 0; i < citations.length; i++) {
+			if(citations[citations.length-1-i]<i+1)
+				return i;
+		}
+        return citations.length;
+    }
+}
