281a749cb10942cba1b6c1b814bca81ab1b2c5b8
alanHarper123 <1531508001@qq.com>
2020-04-12 19:14:54 +0800

    last stone weight BEST

---
+++ src/leetCode/problems/_Last_Stone_Weight/Solution.java
@@ -0,0 +1,29 @@
+package leetCode.problems._Last_Stone_Weight;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.PriorityQueue;
+
+public class Solution {
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+       
+        for (int i = 0; i < stones.length; i++) {
+        	maxHeap.add(stones[i]);
+		}
+        
+        while(maxHeap.size()>=2) {
+        	int max = maxHeap.poll();
+        	int sMax = maxHeap.poll();
+        	if(max-sMax!=0)
+        		maxHeap.add(max-sMax);
+        }
+        
+        if(!maxHeap.isEmpty())
+        	return maxHeap.poll();
+        
+        return 0;
+    }
+}
