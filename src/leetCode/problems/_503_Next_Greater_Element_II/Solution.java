5e4c25405877bd52b0d64c35eb04e030fe354c20
alanHarper123 <1531508001@qq.com>
2020-05-15 18:19:15 +0800

    best than ever

--- src/leetCode/problems/_503_Next_Greater_Element_II/Solution.java
+++ src/leetCode/problems/_503_Next_Greater_Element_II/Solution.java
@@ -3,25 +3,38 @@ package leetCode.problems._503_Next_Greater_Element_II;
 import java.util.LinkedList;
 
 public class Solution {
-    public int[] nextGreaterElements(int[] nums) {
-    	if(nums==null)
-    		return null;
-        int[] result = new int[nums.length];
-        if(nums.length==0)
-    		return result;
-        if(nums.length==1) {
-        	result[0]=-1;
-        	return result;
-        }
-        LinkedList<Integer> unsolvedIndexs = new LinkedList<>();
-        for (int i = 1; i < result.length; i++) {
-			if(nums[i]>nums[i-1]) {
-				result[i-1] = nums[i];
-			}
-			else 
-				unsolvedIndexs.add(i-1);
-			
+	public int[] nextGreaterElements(int[] nums) {
+		if(nums==null)
+			return null;
+		int[] result = new int[nums.length];
+		if(nums.length==0)
+			return result;
+		if(nums.length==1) {
+			result[0]=-1;
+			return result;
 		}
-        
-    }
+		LinkedList<Integer> unsolvedIndexs = new LinkedList<>();
+		for (int i = 0,pre=result.length-1; i < result.length;) {
+			if(nums[i]>nums[pre]) {
+				result[pre] = nums[i];
+				while (unsolvedIndexs.size()>0&&nums[i]>nums[unsolvedIndexs.peekLast()]) {
+					result[unsolvedIndexs.pollLast()] = nums[i];
+				}
+			}else 
+				unsolvedIndexs.add(pre);
+			pre++;
+			i++;
+			if(pre==result.length)
+				pre = 0;
+		}
+		for (int i = 0; i < result.length; i++) {
+			while (unsolvedIndexs.size()>0&&nums[i]>nums[unsolvedIndexs.peekLast()]) {
+				result[unsolvedIndexs.pollLast()] = nums[i];
+			}
+		}
+		while (unsolvedIndexs.size()>0) {
+			result[unsolvedIndexs.pollLast()] = -1;
+		}
+		return result;
+	}
 }
