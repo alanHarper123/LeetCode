848835738e66aaebf82471c56da02de19414762b
alanHarper123 <1531508001@qq.com>
2020-05-27 14:30:36 +0800

    561 best

---
+++ src/leetCode/problems/_561_Array_Partition_I/Solution.java
@@ -0,0 +1,14 @@
+package leetCode.problems._561_Array_Partition_I;
+
+import java.util.Arrays;
+
+public class Solution {
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int sum=0;
+        for (int i = 0; i < nums.length; i+=2) {
+			sum+=nums[i];
+		}
+        return sum;
+    }
+}
