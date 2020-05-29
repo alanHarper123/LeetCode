1e4d7b2e7ec5125f736fe16242881316587b0a01
alanHarper123 <1531508001@qq.com>
2020-05-29 15:14:32 +0800

    590 good

---
+++ src/leetCode/problems/_590_N_ary_Tree_Postorder_Traversal/Solution.java
@@ -0,0 +1,41 @@
+package leetCode.problems._590_N_ary_Tree_Postorder_Traversal;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+
+import leetCode.problems._589_N_ary_Tree_Preorder_Traversal.Node;
+
+//Definition for a Node.
+class Node {
+ public int val;
+ public List<Node> children;
+
+ public Node() {}
+
+ public Node(int _val) {
+     val = _val;
+ }
+
+ public Node(int _val, List<Node> _children) {
+     val = _val;
+     children = _children;
+ }
+};
+
+public class Solution {
+    public List<Integer> postorder(Node root) {
+    	LinkedList<Integer> list = new LinkedList<>();
+    	if(root==null)
+    		return list;
+    	LinkedList<Node> unTraversedNodes = new LinkedList<>();
+    	unTraversedNodes.add(root);
+    	while (!unTraversedNodes.isEmpty()) {
+			Node node = unTraversedNodes.pollLast();
+			list.addFirst(node.val);
+			for(Node child:node.children)
+				unTraversedNodes.add(child);
+		}
+    	return list;
+    }
+}
