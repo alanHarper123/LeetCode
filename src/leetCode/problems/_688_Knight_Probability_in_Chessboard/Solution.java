8ead8978d62fccb5c05b42ba17fac365001c3eba
alanHarper123 <1531508001@qq.com>
2020-06-17 20:01:33 +0800

    688 good

---
+++ src/leetCode/problems/_688_Knight_Probability_in_Chessboard/Solution.java
@@ -0,0 +1,31 @@
+package leetCode.problems._688_Knight_Probability_in_Chessboard;
+
+import java.util.Arrays;
+
+public class Solution {
+	private static int[][] dirs = new int[][] {{2,1},{2,-1},{-2,1},{-2,-1},{1,2},{-1,2},{1,-2},{-1,-2}};
+    public double knightProbability(int N, int K, int r, int c) {
+        double[][] dp = new double[N][N];
+        for(double[] dpR:dp) {
+        	Arrays.fill(dpR, 1);
+        }
+        while (K>0) {
+			K--;
+			double[][] dpC = new double[N][N];
+			int nextPr;
+			int nextPc;
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					for (int[] dir:dirs) {
+						nextPr = i+dir[0];
+						nextPc = j+dir[1];
+						if(nextPc>=0&&nextPc<N&&nextPr>=0&&nextPr<N)
+							dpC[i][j]+=dp[nextPr][nextPc]/8;
+					}
+				}
+			}
+			dp = dpC;
+		}
+        return dp[r][c];
+    }
+}
