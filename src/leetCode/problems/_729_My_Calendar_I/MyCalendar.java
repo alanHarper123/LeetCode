e5d2e8566cf3fa3bc96c1d89e7941d17925ff621
alanHarper123 <1531508001@qq.com>
2020-06-25 16:12:33 +0800

    729 good

---
+++ src/leetCode/problems/_729_My_Calendar_I/MyCalendar.java
@@ -0,0 +1,11 @@
+package leetCode.problems._729_My_Calendar_I;
+
+public class MyCalendar {
+    public MyCalendar() {
+        
+    }
+    
+    public boolean book(int start, int end) {
+        
+    }
+}
