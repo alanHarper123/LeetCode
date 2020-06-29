010fc48946fd89993d62fb10f664d510ba3c0569
alanHarper123 <1531508001@qq.com>
2020-06-29 17:35:39 +0800

    748 good

---
+++ src/leetCode/problems/_748_Shortest_Completing_Word/Solution.java
@@ -0,0 +1,34 @@
+package leetCode.problems._748_Shortest_Completing_Word;
+
+public class Solution {
+    public String shortestCompletingWord(String licensePlate, String[] words) {
+    	int[] keyWord = new int[26];
+    	int charCount = 0;
+    	for (int i = 0; i < licensePlate.length(); i++) {
+			char c = licensePlate.charAt(i);
+			c = Character.toLowerCase(c);
+			if(c>='a'&&c<='z') {
+				keyWord[c-'a']++;
+				charCount++;
+			}	
+		}
+    	String result = null;
+        loop1:for (String word:words) {
+        	if(charCount>word.length()||
+        			(result!=null&&result.length()<=word.length()))
+        		continue;
+			int[] newWord = new int[26];
+			for (int i = 0; i < word.length(); i++) {
+				int index = word.charAt(i)-'a';
+				newWord[index]++;
+			}
+			for (int i = 0; i < keyWord.length; i++) {
+				if(keyWord[i]!=0&&keyWord[i]>newWord[i])
+					continue loop1;
+			}
+			result = word;
+			
+		}
+        return result;
+    }
+}
