56576e86a18fa5a30c1da1bae93e5ad4de214e99
alanHarper123 <1531508001@qq.com>
2020-07-29 16:37:40 +0800

    888 good

---
+++ src/leetCode/problems/_888_Fair_Candy_Swap/Solution.java
@@ -0,0 +1,75 @@
+package leetCode.problems._888_Fair_Candy_Swap;
+
+import java.util.Arrays;
+
+public class Solution {
+    public int[] fairCandySwap(int[] A, int[] B) {
+    	int dif=0;
+    	for (int i = 0; i < A.length; i++) {
+    		dif+=A[i];
+		}
+    	for (int i = 0; i < B.length; i++) {
+    		dif-=B[i];
+		}
+    	dif>>=1;
+        Arrays.sort(A);
+        Arrays.sort(B);
+        int ai = A.length-1;
+        int bi = B.length-1;
+        // similar to two sum;
+        while (true) {
+			if(A[ai]-B[bi]>dif)
+				ai--;
+			else if(A[ai]-B[bi]<dif) {
+				bi--;
+			}else {
+				return new int[] {A[ai],B[bi]};
+			}
+		}
+    }
+}
+/*
+888. Fair Candy Swap
+Easy
+
+Alice and Bob have candy bars of different sizes: A[i] is the size of the i-th bar of candy that Alice has, and B[j] is the size of the j-th bar of candy that Bob has.
+
+Since they are friends, they would like to exchange one candy bar each so that after the exchange, they both have the same total amount of candy.  (The total amount of candy a person has is the sum of the sizes of candy bars they have.)
+
+Return an integer array ans where ans[0] is the size of the candy bar that Alice must exchange, and ans[1] is the size of the candy bar that Bob must exchange.
+
+If there are multiple answers, you may return any one of them.  It is guaranteed an answer exists.
+
+ 
+
+Example 1:
+
+Input: A = [1,1], B = [2,2]
+Output: [1,2]
+
+Example 2:
+
+Input: A = [1,2], B = [2,3]
+Output: [1,2]
+
+Example 3:
+
+Input: A = [2], B = [1,3]
+Output: [2,3]
+
+Example 4:
+
+Input: A = [1,2,5], B = [2,4]
+Output: [5,4]
+
+ 
+
+Note:
+
+    1 <= A.length <= 10000
+    1 <= B.length <= 10000
+    1 <= A[i] <= 100000
+    1 <= B[i] <= 100000
+    It is guaranteed that Alice and Bob have different total amounts of candy.
+    It is guaranteed there exists an answer.
+ */
