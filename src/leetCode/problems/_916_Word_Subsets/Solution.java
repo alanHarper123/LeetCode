d7922303f56c5a87c64a85a3cdd00fd08840d103
alanHarper123 <1531508001@qq.com>
2020-08-06 15:20:05 +0800

    916 good

---
+++ src/leetCode/problems/_916_Word_Subsets/Solution.java
@@ -0,0 +1,85 @@
+package leetCode.problems._916_Word_Subsets;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Solution {
+    public List<String> wordSubsets(String[] A, String[] B) {
+        int[] counts = new int[26];
+        int[] countsR = new int[26];
+        for(String word:B) {
+        	for (int i = 0; i < word.length(); i++) {
+				countsR[word.charAt(i)-'a']++;
+			}
+        	for (int i = 0; i < countsR.length; i++) {
+        		counts[i] = Math.max(counts[i], countsR[i]);
+			}
+        	for (int i = 0; i < countsR.length; i++) {
+        		countsR[i] = 0;
+			}
+        }
+        LinkedList<String> res = new LinkedList<>();
+        loop1:for(String word:A) {
+        	for (int i = 0; i < countsR.length; i++) {
+        		countsR[i] = 0;
+			}
+        	for (int i = 0; i < word.length(); i++) {
+				countsR[word.charAt(i)-'a']++;
+			}
+        	for (int i = 0; i < countsR.length; i++) {
+        		if(counts[i]>countsR[i])
+        			continue loop1;
+			}
+        	res.add(word);
+        }
+        return res;
+    }
+}
+/*
+916. Word Subsets
+Medium
+
+We are given two arrays A and B of words.  Each word is a string of lowercase letters.
+
+Now, say that word b is a subset of word a if every letter in b occurs in a, including multiplicity.  For example, "wrr" is a subset of "warrior", but is not a subset of "world".
+
+Now say a word a from A is universal if for every b in B, b is a subset of a. 
+
+Return a list of all universal words in A.  You can return the words in any order.
+
+ 
+
+Example 1:
+
+Input: A = ["amazon","apple","facebook","google","leetcode"], B = ["e","o"]
+Output: ["facebook","google","leetcode"]
+
+Example 2:
+
+Input: A = ["amazon","apple","facebook","google","leetcode"], B = ["l","e"]
+Output: ["apple","google","leetcode"]
+
+Example 3:
+
+Input: A = ["amazon","apple","facebook","google","leetcode"], B = ["e","oo"]
+Output: ["facebook","google"]
+
+Example 4:
+
+Input: A = ["amazon","apple","facebook","google","leetcode"], B = ["lo","eo"]
+Output: ["google","leetcode"]
+
+Example 5:
+
+Input: A = ["amazon","apple","facebook","google","leetcode"], B = ["ec","oc","ceo"]
+Output: ["facebook","leetcode"]
+
+ 
+
+Note:
+
+    1 <= A.length, B.length <= 10000
+    1 <= A[i].length, B[i].length <= 10
+    A[i] and B[i] consist only of lowercase letters.
+    All words in A[i] are unique: there isn't i != j with A[i] == A[j].
+ */
