10f76018055b863937e6c63f49b80238bd9acbfd
alanHarper123 <1531508001@qq.com>
2020-09-04 14:42:36 +0800

    1023 good

---
+++ src/leetCode/problems/_1023_Camelcase_Matching/Solution.java
@@ -0,0 +1,84 @@
+package leetCode.problems._1023_Camelcase_Matching;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Solution {
+    public List<Boolean> camelMatch(String[] queries, String pattern) {
+        LinkedList<Boolean> res = new LinkedList<>();
+        loop1:for(String query:queries) {
+        	int j=0;
+        	char pc = pattern.charAt(j);
+        	char qc;
+        	int i = 0;
+        	for (; i < query.length(); i++) {
+				qc = query.charAt(i);
+				if(qc==pc) {
+					j++;
+					if(j==pattern.length()) {
+						i++;
+						for (; i < query.length(); i++) {
+							if(query.charAt(i)<='Z') {
+								res.add(false);
+								continue loop1;
+							}
+						}
+						res.add(true);
+						continue loop1;
+					}
+					pc = pattern.charAt(j);
+				}else {
+					if(qc<='Z') {
+						res.add(false);
+						continue loop1;
+					}
+				}
+			}
+        	res.add(false);
+        }
+        return res;
+    }
+}
+/*
+1023. Camelcase Matching
+Medium
+
+A query word matches a given pattern if we can insert lowercase letters to the pattern word so that it equals the query. (We may insert each character at any position, and may insert 0 characters.)
+
+Given a list of queries, and a pattern, return an answer list of booleans, where answer[i] is true if and only if queries[i] matches the pattern.
+
+ 
+
+Example 1:
+
+Input: queries = ["FooBar","FooBarTest","FootBall","FrameBuffer","ForceFeedBack"], pattern = "FB"
+Output: [true,false,true,true,false]
+Explanation: 
+"FooBar" can be generated like this "F" + "oo" + "B" + "ar".
+"FootBall" can be generated like this "F" + "oot" + "B" + "all".
+"FrameBuffer" can be generated like this "F" + "rame" + "B" + "uffer".
+
+Example 2:
+
+Input: queries = ["FooBar","FooBarTest","FootBall","FrameBuffer","ForceFeedBack"], pattern = "FoBa"
+Output: [true,false,true,false,false]
+Explanation: 
+"FooBar" can be generated like this "Fo" + "o" + "Ba" + "r".
+"FootBall" can be generated like this "Fo" + "ot" + "Ba" + "ll".
+
+Example 3:
+
+Input: queries = ["FooBar","FooBarTest","FootBall","FrameBuffer","ForceFeedBack"], pattern = "FoBaT"
+Output: [false,true,false,false,false]
+Explanation: 
+"FooBarTest" can be generated like this "Fo" + "o" + "Ba" + "r" + "T" + "est".
+
+ 
+
+Note:
+
+    1 <= queries.length <= 100
+    1 <= queries[i].length <= 100
+    1 <= pattern.length <= 100
+    All strings consists only of lower and upper case English letters.
+ */
